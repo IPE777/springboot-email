8d9999efd4c24a09189480361a7ed0ce4886062f
wangyuhang <1136361325@qq.com>
2022-03-05 19:06:33 +0800

    first

---
+++ src/main/java/com/wangyuhang/SpringbootEmailApplication.java
@@ -0,0 +1,15 @@
+package com.wangyuhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+public class SpringbootEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootEmailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wangyuhang/springboot_email/SpringbootEmailApplicationTests.java
@@ -0,0 +1,54 @@
+package com.wangyuhang.springboot_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@SpringBootTest
+class SpringbootEmailApplicationTests {
+
+    @Autowired
+    JavaMailSenderImpl mailSender;
+
+    @Test
+    public void contextLoads() {
+        //邮件设置1：一个简单的邮件
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setSubject("通知-课");
+        message.setText("今晚7:30开会");
+
+        message.setTo("1136361325@qq.com");
+        message.setFrom("1136361325@qq.com");
+        for(int i=1;i<10;i++){
+
+
+            mailSender.send(message);
+        }
+        @Test
+        public void contextLoads2() throws MessagingException {
+            //邮件设置2：一个复杂的邮件
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+            helper.setSubject("通知-明天来狂神这听课");
+            helper.setText("<b style='color:red'>今天 7:30来开会</b>",true);
+
+            //发送附件
+            helper.addAttachment("1.jpg",new File(""));
+            helper.addAttachment("2.jpg",new File(""));
+
+            helper.setTo("1136361325@qq.com");
+            helper.setFrom("1136361325@qq.com");
+
+            mailSender.send(mimeMessage);
+        }
+
+    }
+}
